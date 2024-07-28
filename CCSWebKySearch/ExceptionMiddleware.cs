08c9c478312836c6c1f875dd8106486a4d0d834d
Juan R. Cifuentes <juanr.cifuentes@icloud.com>
2024-07-28 13:57:03 -0500

    chore: not using iLogger

--- CCSWebKySearch/ExceptionMiddleware.cs
+++ CCSWebKySearch/ExceptionMiddleware.cs
@@ -5,12 +5,14 @@ using System.Text.Json;
 public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger _logger;
+    // private readonly ILogger _logger;
 
-    public ExceptionMiddleware(RequestDelegate next, ILogger logger)
+    public ExceptionMiddleware(RequestDelegate next, 
+    // ILogger logger
+    )
     {
         _next = next;
-        _logger = logger;
+        // _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -36,29 +38,29 @@ public class ExceptionMiddleware
             case InvalidInputException _:
                 status = HttpStatusCode.BadRequest;
                 message = exception.Message;
-                _logger.LogWarning(exception, "Invalid input: {Message}", exception.Message);
+                // _logger.LogWarning(exception, "Invalid input: {Message}", exception.Message);
                 break;
             case NotFoundException _:
                 status = HttpStatusCode.NotFound;
                 message = exception.Message;
-                _logger.LogWarning(exception, "Not found: {Message}", exception.Message);
+                // _logger.LogWarning(exception, "Not found: {Message}", exception.Message);
                 break;
             case BadHttpRequestException _:
                 status = HttpStatusCode.BadRequest;
                 message = "Invalid request data.";
-                _logger.LogWarning(exception, "Bad request: {Message}", exception.Message);
+                // _logger.LogWarning(exception, "Bad request: {Message}", exception.Message);
                 break;
 
             case FileNotFoundException _:
                 status = HttpStatusCode.BadRequest;
                 message = "Invalid request data, not file found.";
-                _logger.LogWarning(exception, "Bad request: {Message}", exception.Message);
+                // _logger.LogWarning(exception, "Bad request: {Message}", exception.Message);
                 break;
                 
             default:
                 status = HttpStatusCode.InternalServerError;
                 message = "An unexpected error occurred.";
-                _logger.LogError(exception, "Unexpected error: {Message}", exception.Message);
+                // _logger.LogError(exception, "Unexpected error: {Message}", exception.Message);
                 break;
         }
 
